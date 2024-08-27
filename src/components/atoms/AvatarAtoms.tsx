305291efb03b880898db832776b2ed8f92f5f204
Yogawan Aditya Pratama <123430193+yogawan@users.noreply.github.com>
2024-08-27 10:16:42 +0700

    fix erorr

--- src/components/atoms/AvatarAtoms.tsx
+++ src/components/atoms/AvatarAtoms.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+// import React from 'react';
 
-const AvatarAtoms = ({ src, alt }) => {
-  return <img src={src} alt={alt} className="avatar" />;
-};
+// const AvatarAtoms = ({ src, alt }) => {
+//   return <img src={src} alt={alt} className="avatar" />;
+// };
 
-export default AvatarAtoms;
+// export default AvatarAtoms;
